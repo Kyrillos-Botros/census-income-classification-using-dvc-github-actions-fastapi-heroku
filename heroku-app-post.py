b94833194dfb3655ca1d723daa25f9c04e62f7fb
kyrillos botros <kyrillos.zakher00@gmail.com>
2023-07-20 00:48:09 +0300

    adding heoku-post-code and deployment screenshots

---
+++ heroku-app-post.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+url= "https://census-classification-app-beee7d84c1fe.herokuapp.com/predict"
+
+body= {"path": "data/test-data.csv"}
+
+r = requests.post(url, data=json.dumps(body))
+print("status code is", r.status_code)
+print("predictions are", r.json())
